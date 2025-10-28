9a23da304d1c0c55382815f8b0b9155d8edcc2ab
Anna Filonenko <filonenkoanna06aa@gmail.com>
2025-10-28 17:58:25 +0200

    primer1 sem4

---
+++ SEMINARI/primer1 sem4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+	if (num <= 1)
+		return false;
+	for (int i = 2; i < num; i++)
+	{
+		num% i;
+		if (num % i == 0)
+			return false;
+	}
+	return true;
+}
+void result(int num) {
+	for (int j = 1; j <= num; j++) {
+		int k1 = 6 * j + 1;
+		int k2 = 6 * j - 1;
+		cout << "k1: " << k1 << " k2: " << k2 << endl;
+	}
+}
+int main() {
+	int n;
+	cin >> n;
+	isPrime(n);
+	if (isPrime(n))
+		result(n);
+
+}
+
+
+void reverseNumber(int num)() {
+	int reverse;
+	while (num / 10 > 0) {
+		int digit = 0;
+		digit = num % 10
+	}
+}
